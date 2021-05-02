9e14f763575f69e5ff02afbc1da915a0284121fc
rishabh-dev <rishavraj12262@gmail.com>
2021-05-02 22:13:18 +0530

    origin

---
+++ starstudioo/src/App.js
@@ -0,0 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import './App.css';
+import Home from './base/home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home /> 
+        </Route>
+        <Route  path="/about">
+          <h2>ABOUT AREA</h2>
+        </Route>
+        <Route path="*">
+          <h2>NOT FOUND</h2>
+        </Route>
+      </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
